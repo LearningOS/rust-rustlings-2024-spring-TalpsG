806186b6cdf122b96d2a93a8870a5d2fbf2043bb
TalpsG <135484898+TalpsG@users.noreply.github.com>
2024-04-17 10:09:41 +0000

    100

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        let name = v[0].to_string();
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+        let age = v[1].parse::<usize>();
+        if let Err(e) = age {
+            return Err(ParsePersonError::ParseInt(e));
+        }
+        let age = age.unwrap();
+        Ok(Person { name, age })
+
     }
 }
 
--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,7 +27,6 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
@@ -36,8 +35,11 @@ extern "Rust" {
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    pub fn my_demo_function(a: u32) -> u32 {
         a
+    }
+    pub fn my_demo_function_alias(a:u32)->u32{
+        my_demo_function(a)
     }
 }
 
@@ -54,8 +56,8 @@ mod tests {
         // SAFETY: We know those functions are aliases of a safe
         // Rust function.
         unsafe {
-            my_demo_function(123);
-            my_demo_function_alias(456);
+            Foo::my_demo_function(123);
+            Foo::my_demo_function_alias(456);
         }
     }
 }
